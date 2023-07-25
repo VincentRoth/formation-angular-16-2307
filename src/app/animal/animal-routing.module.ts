92fa48de9ad34d1f880ae2514a538862ec89474f
Vincent Roth <vincent@placeme.io>
2023-07-25 16:33:36 +0200

    Routes enfants

--- src/app/animal/animal-routing.module.ts
+++ src/app/animal/animal-routing.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
+import { AnimalComponent } from './animal.component';
 
 const routes: Routes = [
   {
     path: 'animals',
-    component: AnimalListComponent,
-    title: 'Listes des animaux',
-  },
-  {
-    path: 'animals/:id',
-    component: AnimalDetailComponent,
-    title: "Fiche d'un animal",
+    component: AnimalComponent,
+    children: [
+      {
+        path: '',
+        component: AnimalListComponent,
+        title: 'Listes des animaux',
+      },
+      {
+        path: ':id',
+        component: AnimalDetailComponent,
+        title: "Fiche d'un animal",
+      },
+    ],
   },
 ];
 
